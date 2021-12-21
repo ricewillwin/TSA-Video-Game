8ef1418270c6bf55efa660f122b4baedaf11ba0a
William Rice <ricewilliam125@gmail.com>
2021-12-21 16:00:33 -0500

    Started Backend

---
+++ index.js
@@ -0,0 +1,33 @@
+kaboom({
+  global: true,
+  fullscreen: true,
+  scale: 0.6,
+  debug: true
+})
+
+scene("menu", () => {
+
+	add([
+		text("Snake game"),
+		pos(240, 80),
+		scale(3),
+	]);
+
+	add([
+		rect(160, 20),
+		pos(240, 180),
+		"button",
+		{
+			clickAction: () => go('game'),
+		},
+	]);
+
+	add([
+		text("Play game"),
+		pos(240, 180),
+		color(0, 0, 0)
+	]);
+
+});
+
+start("menu");
