7db8d4632e6c36e617cecd157b2fd0ce0740cbc2
William Rice <ricewilliam125@gmail.com>
2021-12-23 07:53:01 -0500

    added basic menu

--- index.js
+++ index.js
@@ -7,9 +7,51 @@ kaboom({
 
 scene("menu", () => {
 
+  layers([
+    "bg",
+    "game",
+    "ui",
+    "button",
+    "text",
+], "text")
+
+  const menuTitle = add([
+    text("iSpy"),
+    layer("text"),
+    origin("center"),
+    pos(width()/2, height()/4),
+    scale(2),
+    "menuText",
+  ])
+
+  const playButtonText = add([
+    text("play"),
+    layer("text"),
+    color(0, 0, 255),
+    origin("center"),
+    pos(width()/2, height()/4 + menuTitle.height + 100),
+    onClick(() => go("game")),
+    "playButtonText",
+  ])
+
+  const playButton = add([
+    rect(playButtonText.width, playButtonText.height),
+    layer("button"),
+    color(1, 1, 1),
+    origin("center"),
+    width(playButtonText.width),
+    height(playButtonText.height),
+    area(),
+    pos(width()/2, height()/4 + menuTitle.height + 100),
+    "playButton",
+  ])
+
+})
+
+scene("game", () => {
+
   add([
-    text("Hello"),
-    pos(200,100)
+    text("gamer")
   ])
 
 })
