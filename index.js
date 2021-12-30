485b3d9e464d0c32b299598952e95b6e89a8e775
AiDorneich <dorneich.aiden@gmail.com>
2021-12-30 14:38:41 -0500

    Changed kaboom context to global: false

--- index.js
+++ index.js
@@ -1,147 +1,138 @@
-import k from "./kaboom.js"
-import sceneOne from "./Levels/level1.js"
+import { k } from "./kaboom.js";
+import { loadLevel1 } from "./Levels/level1.js";
 
-const buttonPadding = 50
-loadSprite("background", "./Tiles/mainBackground.png")
+const buttonPadding = 50;
+k.loadSprite("background", "./Tiles/mainBackground.png");
+loadLevel1();
 
-scene("menu", () => {
+k.scene("menu", () => {
 
-  add([
-    sprite("background"),
-    origin("center"),
-    pos(width()/2, height()/2),
-    layer("bg"),
-    area(),
-    scale(3.75),
-  ])
+  k.add([
+    k.sprite("background"),
+    k.origin("center"),
+    k.pos(k.width()/2, k.height()/2),
+    k.layer("bg"),
+    k.area(),
+    k.scale(3.75),
+  ]);
 
-  const menuTitle = add([
-    text("iSpy"),
-    layer("ui"),
-    origin("center"),
-    pos(width()/2, height()/4),
-    scale(2),
+  const menuTitle = k.add([
+    k.text(" iSpy "),
+    k.layer("ui"),
+    k.origin("center"),
+    k.pos(k.width()/2, k.height()/4),
+    k.scale(2),
     "menuText",
-  ])
+  ]);
 
-  const playButtonText = add([
-    layer("ui"),
-    text("play"),
-    z(1),
-    color(0, 0, 255),
-    origin("center"),
-    pos(width()/2, height()/4 + menuTitle.height + 100),
+  const playButtonText = k.add([
+    k.layer("ui"),
+    k.text("play"),
+    k.z(1),
+    k.color(0, 0, 255),
+    k.origin("center"),
+    k.pos(k.width()/2, k.height()/4 + menuTitle.height + 100),
     "playButtonText",
-  ])
+  ]);
 
-  const playButton = add([
-    rect(playButtonText.width + buttonPadding, playButtonText.height + buttonPadding),
-    layer("ui"),
-    color(1, 1, 1),
-    origin("center"),
-    area(),
-    pos(width()/2, height()/4 + menuTitle.height + 100),
+  const playButton = k.add([
+    k.rect(playButtonText.width + buttonPadding, playButtonText.height + buttonPadding),
+    k.layer("ui"),
+    k.color(1, 1, 1),
+    k.origin("center"),
+    k.area(),
+    k.pos(k.width()/2, k.height()/4 + menuTitle.height + 100),
     "playButton",
-  ])
+  ]);
 
-  onClick("playButton", () => go("game"))
+  k.onClick("playButton", () => k.go("game"));
 
-})
+});
 
 // Start the actual game
 // 
 // Cut scene then pick which situation
 // After picking starter go to first level
-scene("game", () => {
+k.scene("game", () => {
 
-  add([
-    text("situation"),
-    pos(width()/2, height()/4),
-    origin("center"),
-    scale(0.8),
-    "situationText"
-  ])
+  k.add([
+    k.text("situation"),
+    k.pos(k.width()/2, k.height()/4),
+    k.origin("center"),
+    k.scale(0.8),
+    "situationText",
+  ]);
 
-
-
-
-
-  const situationOneText = add([
-    text("one"),
-    z(1),
-    layer("ui"),
-    color(0, 0, 255),
-    origin("center"),
-    pos(width()/4, height()/2),
-    scale(0.5),
+  const situationOneText = k.add([
+    k.text("one"),
+    k.z(1),
+    k.layer("ui"),
+    k.color(0, 0, 255),
+    k.origin("center"),
+    k.pos(k.width()/4, k.height()/2),
+    k.scale(0.5),
     "situationOneText",
-  ])
+  ]);
 
-  const situationOne = add([
-    rect(situationOneText.width + buttonPadding, situationOneText.height + buttonPadding),
-    layer("ui"),
-    color(1, 1, 1),
-    origin("center"),
-    scale(0.5),
-    area(),
-    pos(width()/4, height()/2),
+  const situationOne = k.add([
+    k.rect(situationOneText.width + buttonPadding, situationOneText.height + buttonPadding),
+    k.layer("ui"),
+    k.color(1, 1, 1),
+    k.origin("center"),
+    k.scale(0.5),
+    k.area(),
+    k.pos(k.width()/4, k.height()/2),
     "situationOne",
-  ])
+  ]);
 
-  onClick("situationOne", () => go("level1"))
-
-
+  k.onClick("situationOne", () => k.go("level1"));
 
 
-
-  const situationTwoText = add([
-    text("two"),
-    z(1),
-    layer("ui"),
-    color(0, 0, 255),
-    scale(0.5),
-    origin("center"),
-    pos(width()/2, height()/2),
+  const situationTwoText = k.add([
+    k.text("two"),
+    k.z(1),
+    k.layer("ui"),
+    k.color(0, 0, 255),
+    k.scale(0.5),
+    k.origin("center"),
+    k.pos(k.width()/2, k.height()/2),
     "situationTwoText",
-  ])
+  ]);
 
-  const situationTwo = add([
-    rect(situationTwoText.width + buttonPadding, situationTwoText.height + buttonPadding),
-    layer("ui"),
-    color(1, 1, 1),
-    scale(0.5),
-    origin("center"),
-    area(),
-    pos(width()/2, height()/2),
+  const situationTwo = k.add([
+    k.rect(situationTwoText.width + buttonPadding, situationTwoText.height + buttonPadding),
+    k.layer("ui"),
+    k.color(1, 1, 1),
+    k.scale(0.5),
+    k.origin("center"),
+    k.area(),
+    k.pos(k.width()/2, k.height()/2),
     "situationTwo",
-  ])
+  ]);
 
 
-
-
-
-  const situationThreeText = add([
-    text("three"),
-    z(1),
-    scale(0.5),
-    layer("ui"),
-    color(0, 0, 255),
-    origin("center"),
-    pos(width()/4 + width()/2, height()/2),
+  const situationThreeText = k.add([
+    k.text("three"),
+    k.z(1),
+    k.scale(0.5),
+    k.layer("ui"),
+    k.color(0, 0, 255),
+    k.origin("center"),
+    k.pos(k.width()/4 + k.width()/2, k.height()/2),
     "situationThreeText",
-  ])
+  ]);
 
-  const situationThree = add([
-    rect(situationThreeText.width + buttonPadding, situationThreeText.height + buttonPadding),
-    layer("ui"),
-    color(1, 1, 1),
-    origin("center"),
-    scale(0.5),
-    area(),
-    pos(width()/4 + width()/2, height()/2),
+  const situationThree = k.add([
+    k.rect(situationThreeText.width + buttonPadding, situationThreeText.height + buttonPadding),
+    k.layer("ui"),
+    k.color(1, 1, 1),
+    k.origin("center"),
+    k.scale(0.5),
+    k.area(),
+    k.pos(k.width()/4 + k.width()/2, k.height()/2),
     "situationThree",
-  ])
+  ]);
 
-})
+});
 
-go("menu");
+k.go("menu");
--- kaboom.js
+++ kaboom.js
@@ -1,20 +1,20 @@
-import config from "./config.js"
+import config from "./config.js";
 
-export const k = kaboom({
+const kaboomOpts = {
   background: [134, 135, 247],
-  global: true,
+  global: false,
   height: config.gameHeight,
   width: config.gameWidth,
   stretch: true,
   scale: 1,
   debug: true,
   canvas: document.querySelector("#game"),
-})
+};
+
+export const k = kaboom(kaboomOpts);
 
 k.layers([
-	"bg",
-	"game",
-	"ui",
-], "game")
-
-export default k
+  "bg",
+  "game",
+  "ui",
+], "game");
